8ba520d797e625b542a6b281f9ccb85218529d6f
Janis Bürkle <bks@teamtechnik.com>
2024-04-26 12:48:57 +0200

    Aufgeräumt

--- UNO_Spielprojekt/MultiplayerRooms/Rooms.cs
+++ UNO_Spielprojekt/MultiplayerRooms/Rooms.cs
@@ -9,10 +9,9 @@ public class Rooms : ViewModelBase
 {
     [JsonProperty("Id")] public long Id { get; set; }
     [JsonProperty("RoomName")] public string RoomName { get; set; }
-
     [JsonProperty("PlayButtonEnabled")] public bool PlayButtonEnabled { get; set; } = true;
+    
     private string _playButtonContent = "Play";
-
     [JsonProperty("PlayButtonContent")]
     public string PlayButtonContent
     {
@@ -28,7 +27,6 @@ public class Rooms : ViewModelBase
     }
 
     private int _onlineUsers;
-
     [JsonProperty("OnlineUsers")]
     public int OnlineUsers
     {
@@ -45,7 +43,6 @@ public class Rooms : ViewModelBase
 
 
     private int _maximalUsers;
-
     [JsonProperty("MaximalUsers")]
     public int MaximalUsers
     {
@@ -63,16 +60,11 @@ public class Rooms : ViewModelBase
     [JsonProperty("PasswordSecured")] public bool PasswordSecured { get; set; }
     [JsonProperty("Password")] public string Password { get; set; }
 
-    [JsonProperty("_players")]
-    private ObservableCollection<MultiplayerPlayer> _players = new ObservableCollection<MultiplayerPlayer>();
-    
-    [JsonProperty("MiddleCard")]
-    public CardDTO MiddleCard { get; set; }
-    [JsonProperty("MiddleCardPic")]
-    public string MiddleCardPic { get; set; }
-    [JsonProperty("SelectedCard")]
-    public CardDTO SelectedCard { get; set; }   
+    [JsonProperty("MiddleCard")] public CardDTO MiddleCard { get; set; }
+    [JsonProperty("MiddleCardPic")] public string MiddleCardPic { get; set; }
+    [JsonProperty("SelectedCard")] public CardDTO SelectedCard { get; set; }
 
+    private ObservableCollection<MultiplayerPlayer> _players = new ObservableCollection<MultiplayerPlayer>();
     [JsonProperty("Players")]
     public ObservableCollection<MultiplayerPlayer> Players
     {
@@ -87,8 +79,7 @@ public class Rooms : ViewModelBase
         }
     }
 
-    [JsonProperty("_cards")] private ObservableCollection<CardViewModel> _cards = new();
-
+    private ObservableCollection<CardViewModel> _cards = new();
     [JsonProperty("Cards")]
     public ObservableCollection<CardViewModel> Cards
     {
